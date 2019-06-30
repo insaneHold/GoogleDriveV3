7949f409466fe3357c72cad22665f95c592929d1
insaneHold <43003493+insaneHold@users.noreply.github.com>
2019-06-30 09:58:27 -0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.FilePath = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(139, 116)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(170, 25)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Upload On Google Drive "
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(44, 23)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(73, 25)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Browser"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'FilePath
+        '
+        Me.FilePath.Location = New System.Drawing.Point(139, 78)
+        Me.FilePath.Name = "FilePath"
+        Me.FilePath.Size = New System.Drawing.Size(134, 20)
+        Me.FilePath.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(460, 199)
+        Me.Controls.Add(Me.FilePath)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents FilePath As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,49 @@
+﻿Imports Google.Apis.Auth.OAuth2
+Imports Google.Apis.Drive.v3
+Imports Google.Apis.Drive.v3.Data
+Imports Google.Apis.Services
+Imports System.IO
+Imports System.Threading
+
+Public Class Form1
+    Dim service As New DriveService
+    Private Sub createservice()
+        Dim clientid = "1029816439958-7r4jgk9m205mroj45dgulu4hjvh40tg5.apps.googleusercontent.com"
+        Dim clientsecret = "qCvG6vveD3bFfOOOAHGoZVSb"
+
+        Dim uc As UserCredential = GoogleWebAuthorizationBroker.AuthorizeAsync(New ClientSecrets() With {.ClientId = clientid, .ClientSecret = clientsecret}, {DriveService.Scope.Drive}, "user", CancellationToken.None).Result
+        service = New DriveService(New BaseClientService.Initializer() With {.HttpClientInitializer = uc, .ApplicationName = "Google Drive"})
+
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim f As New OpenFileDialog
+        If f.ShowDialog = DialogResult.OK Then
+            FilePath.Text = f.FileName
+        Else
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Cursor = Cursors.WaitCursor
+        If service.ApplicationName <> "Google Drive" Then createservice()
+        Dim thefile As New Data.File
+        thefile.Name = "Triptico"
+        thefile.Description = "A test document"
+        thefile.MimeType = "text/plain"
+        Dim bytearry As Byte() = System.IO.File.ReadAllBytes(FilePath.Text)
+        Dim stream As New System.IO.MemoryStream(bytearry)
+        Dim UploadRequest As FilesResource.CreateMediaUpload = service.Files.Create(thefile, stream, thefile.MimeType)
+        Dim Response = UploadRequest.Upload()
+        Dim file As Data.File = uploadrequest.ResponseBody
+        Me.Cursor = Cursors.Default
+        MessageBox.Show(" Upload Successful ")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FilePath.Text = "c:/backup/evea.bak"
+        createservice()
+    End Sub
+End Class
